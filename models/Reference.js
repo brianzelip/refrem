45acb3c238018cef1e37be04f6e0cf0abcc1423e
Brian Zelip <bzelip@gmail.com>
2017-10-12 07:13:58 -0400

    still cannot get timestamps working

--- models/Reference.js
+++ models/Reference.js
@@ -2,23 +2,27 @@ const mongoose = require('mongoose');
 // have to tell mongoose that we're using ES6 async/await
 mongoose.Promise = global.Promise;
 
-const referenceSchema = new mongoose.Schema({
-  refUrl: {
-    type: String,
-    trim: true
+const referenceSchema = new mongoose.Schema(
+  {
+    refUrl: {
+      type: String,
+      trim: true
+    },
+    commentsUrl: {
+      type: String,
+      trim: true
+    },
+    source: String,
+    category: String
   },
-  commentsUrl: {
-    type: String,
-    trim: true
-  },
-  source: String,
-  category: String,
-  timeStamp: String
-});
+  {
+    timestamps: true
+  }
+);
 
-referenceSchema.pre('save', function(next) {
-  this.timeStamp = 'hello';
-  next();
-}); // needs to be a long-form function because we need `this`, so arrow func won't do
+// referenceSchema.pre('save', function(next) {
+//   this.timeStamp = Date.now();
+//   next();
+// }); // needs to be a long-form function because we need `this`, so arrow func won't do
 
 module.exports = mongoose.model('Store', referenceSchema);
