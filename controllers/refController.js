5ba549a79e8e35c683651a266437394a57440d86
Brian Zelip <bzelip@gmail.com>
2017-10-13 07:05:33 -0400

    add head mixin and pass data along for custom head metadata

--- controllers/refController.js
+++ controllers/refController.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 const Reference = mongoose.model('Reference');
 
 exports.homePage = (req, res) => {
-  res.render('index', { title: 'Add a Reference to Remember' });
+  res.render('index', {
+    title: 'Add a Reference to Remember',
+    css: 'dist/refrem.css',
+    author: 'Brian Zelip',
+    description: 'to be or not to be',
+    keywords: ['isolate', 'rejoice', 'Moose Bug']
+  });
 };
 
 exports.addReference = async (req, res) => {
