edbdac91b5825ba5188c09c794bab9589aace613
Brian Zelip <bzelip@protonmail.com>
2021-02-27 05:34:54 -0500

    Log app url on start for easy click open

--- start.js
+++ start.js
@@ -29,5 +29,5 @@ require('./models/Reference');
 const app = require('./app');
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
-  console.log(`Express running → PORT ${server.address().port}`);
+  console.log(`Express running → http://localhost:${server.address().port}`);
 });
